5564c78f69470cb5e22f70a13c33ad20054b10bf
Christophe VG <contact@christophe.vg>
2019-12-03 18:08:59 +0100

    dropping support for python2

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ CLASSIFIERS      = [
   "Topic :: Topic :: Software Development",
   "MIT License :: OSI Approved :: MIT License",
   "Programming Language :: Python",
-  "Programming Language :: Python :: 2.7",
   "Programming Language :: Python :: 3.7",
   
 ]
