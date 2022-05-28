dac9a92094c05cc7b371e6be072df7c837b5bbca
Christophe VG <contact@christophe.vg>
2022-05-28 17:03:15 +0200

    fixed a lot of lint issue

--- pypi_template/__main__.py
+++ pypi_template/__main__.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import argparse
 
 from pkg_resources import resource_string, resource_listdir, resource_isdir
 
@@ -10,24 +11,22 @@ import yaml
 from prompt_toolkit.completion import FuzzyWordCompleter
 from prompt_toolkit import prompt
 from prompt_toolkit.styles import Style
-
-style = Style.from_dict({
-  "underlined": "underline"
-})
-
 from colorama import init, Fore
-init(autoreset=True)
 
 from pypi_template import __version__
 
-class CLI(object):
+style = Style.from_dict({ "underlined": "underline" })
+init(autoreset=True)
+
+class CLI():
+
   def __init__(self, path=None, verbose=False, debug=False, edit=None, yes=False):
-    if not path is None: os.chdir(path)
+    if not path is None:
+      os.chdir(path)
     self.be_verbose = verbose
     self.show_debug = debug
     self.edit       = edit
     self.yes        = yes
-    
     self.environment = Environment(
       loader=PackageLoader("pypi_template", "templates")
     )
@@ -46,8 +45,9 @@ class CLI(object):
     self.template_vars = {}
     self.templates     = {}
 
-  def debug(self, msg):
-    if self.show_debug: print(msg)
+  def verbose(self, msg):
+    if self.be_verbose or self.show_debug:
+      print(msg)
 
   def load_classifiers(self):
     return str(
@@ -58,31 +58,34 @@ class CLI(object):
     return resource_string(__name__, os.path.join("templates", *args))
 
   def list_resources(self, package="pypi_template.templates"):
-    EXCLUDED_EXT = ".pyc"
-    EXCLUDED     = [ "__init__.py", "__pycache__" ]
+    excluded_ext = ".pyc"
+    excluded     = [ "__init__.py", "__pycache__" ]
     files = []
     for resource in resource_listdir(package, ""):
-      if resource.endswith(EXCLUDED_EXT) or resource in EXCLUDED:
+      if resource.endswith(excluded_ext) or resource in excluded:
         pass
       elif resource_isdir(package, resource):
-        subfiles = self.list_resources("{0}.{1}".format(package, resource))
+        subfiles = self.list_resources(f"{package}.{resource}")
         files += [ os.path.join(resource, f) for f in subfiles ]
       else:
         files.append(resource)
     return files
 
   def load_vars(self):
-    try:    self.template_vars = yaml.safe_load(open(".pypi-template"))
-    except: pass
+    try:
+      with open(".pypi-template", encoding="utf-8") as fp:
+        self.template_vars = yaml.safe_load(fp)
+    except:
+      pass
 
   def collect_templates(self):
     for resource in self.list_resources():
       name = resource.replace("(dot)", ".")
       self.templates[name] = self.environment.get_template(resource)
       # extract template variables
-      source = self.load_resource(resource) 
+      source = self.load_resource(resource)
       for var in meta.find_undeclared_variables(self.environment.parse(source)):
-        self.debug("found variable {} : {}".format(name, var))
+        self.verbose(f"found variable {name} : {var}")
         if not var in self.template_vars and not var in self.system_vars:
           self.template_vars[var] = None
 
@@ -93,37 +96,41 @@ class CLI(object):
   def collect_var(self, var, force=False):
     try:
       current = self.template_vars[var]
-      if not force and not current is None and self.yes: return
+      if not force and not current is None and self.yes:
+        return
       if var in self.list_vars:
         self.collect_var_selections(var, current)
       else:
         self.collect_var_value(var, current)
     except KeyError:
-      print("unknown template variable: {0}".format(var))
+      print(f"unknown template variable: {var}")
 
   def collect_var_value(self, var, current):
-    question = "{0}: ".format(var.replace("_", " ").capitalize())
-    if current is None: current = ""
+    question = f"{var.replace('_', ' ').capitalize()}: "
+    if current is None:
+      current = ""
     self.template_vars[var] = prompt(
       [("class:underlined", question)], style=style, default=current
     )
 
-  def collect_var_selections(self, var, current=[]):
-    if not current: current = []
+  def collect_var_selections(self, var, current=None):
+    if not current:
+      current = []
     if len(current) > 0:
-      print(Fore.BLUE + "Current {}:".format(var.replace("_", " ")))
+      print(Fore.BLUE + f"Current {var.replace('_', ' ')}:")
       for selection in current:
-        print(Fore.BLUE + "- {0}".format(selection))
-    question = "Select {0}: ".format(var.replace("_", " "))
+        print(Fore.BLUE + f"- {selection}")
+    question = f"Select {var.replace('_', ' ')}: "
     values   = self.list_vars[var]
-    if values: completer = FuzzyWordCompleter(values)
+    if values:
+      completer = FuzzyWordCompleter(values)
     selections = current
     selection = None
     while selection != "":
       if values:
         selection = prompt(
           [("class:underlined", question)],
-          style=style, 
+          style=style,
           completer=completer,
           complete_while_typing=True
         )
@@ -135,49 +142,64 @@ class CLI(object):
     self.template_vars[var] = selections
 
   def save_var_values(self):
-    with open(".pypi-template", "w") as outfile:
+    with open(".pypi-template", "w", encoding="utf-8") as outfile:
       yaml.safe_dump(self.template_vars, outfile, default_flow_style=False)
 
   def render_files(self):
-    EXCLUDED = [ "base/index.md", "base/classifiers.txt" ]
+    excluded = [ "base/index.md", "base/classifiers.txt" ]
     for filename, template in self.templates.items():
-      if filename in EXCLUDED:
-        self.debug("not rendering {}".format(filename))
+      if filename in excluded:
+        self.verbose(f"not rendering excluded {filename}")
         continue
-        
       directory = os.path.dirname(filename)
       if "skip" in self.template_vars:
         if filename in self.template_vars["skip"] or directory in self.template_vars["skip"]:
-          self.debug("skipping {0}".format(filename))
+          self.verbose(f"skipping {filename}")
           continue
-
       # TODO generalize?
       filename = filename.replace(
         "(package_module_name)", self.template_vars["package_module_name"]
       )
       directory = os.path.dirname(filename)
       if directory != "" and not os.path.exists(directory):
-        self.debug("creating directory {}".format(directory))
-        if not self.show_debug:
-          os.makedirs(directory)
-          # TODO generalize?
-          if directory == self.template_vars["package_module_name"]:
-            with open(os.path.join(directory, "__init__.py"), "w") as f:
-              f.write('__version__ = "0.0.1"')
-      vars = self.template_vars.copy()
-      vars.update(self.system_vars)
-      new_content = template.render(**vars)
+        self._mkdir(directory)
+      applied_vars = self.template_vars.copy()
+      applied_vars.update(self.system_vars)
+      new_content = template.render(**applied_vars)
       if os.path.isfile(filename):
-        with open(filename, "r") as file: original_content = file.read()
+        with open(filename, encoding="utf-8") as file:
+          original_content = file.read()
         if new_content == original_content:
-          self.debug("unchanged {}".format(filename))
+          self.verbose(f"unchanged {filename}")
           continue
-        print("backing up {0}".format(filename))
-        if not self.show_debug:
-          os.rename(filename, filename + ".backup")
-      print("writing {0}".format(filename))
-      if not self.show_debug:
-        with open(filename, "w") as outfile: outfile.write(new_content)
+        self._backup(filename)
+      self._write_file(filename, new_content)
+
+  def _mkdir(self, directory):
+    if self.show_debug:
+      print(f"not really creating directory {directory}")
+    else:
+      self.verbose(f"creating directory {directory}")
+      os.makedirs(directory)
+      # TODO generalize?
+      if directory == self.template_vars["package_module_name"]:
+        with open(os.path.join(directory, "__init__.py"), "w", encoding="utf-8") as fp:
+          fp.write('__version__ = "0.0.1"')
+
+  def _backup(self, filename):
+    if self.show_debug:
+      print(f"not really backing up {filename}")
+    else:
+      self.verbose(f"backing up {filename}")
+      os.rename(filename, filename + ".backup")
+
+  def _write_file(self, filename, new_content):
+    if self.show_debug:
+      print(f"not really writing {filename}")
+    else:
+      self.verbose(f"writing {filename}")
+      with open(filename, "w", encoding="utf-8") as outfile:
+        outfile.write(new_content)
 
   def run(self):
     try:
@@ -192,8 +214,6 @@ class CLI(object):
     except KeyboardInterrupt:
       pass
 
-import argparse
-
 def cli():
   parser = argparse.ArgumentParser(description="Manage a Python PyPi module.")
   parser.add_argument("path", type=str, nargs="?",
@@ -206,7 +226,6 @@ def cli():
                       help="don't do it, just say it")
   parser.add_argument("--verbose", "-v", dest="verbose", action="store_true",
                       help="do it and say it")
-
   args = parser.parse_args()
   CLI(**vars(args)).run()
 
